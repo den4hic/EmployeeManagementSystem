2bc6ce4e5b3b6ed0052b20320f14fc2390b3ee18
Denys <gusardavydov2004@gmail.com>
2024-10-03 22:42:03 +0300

    fix problem with home page, when refresh token hasn't returned response

--- UI/src/app/guards/auth.guard.ts
+++ UI/src/app/guards/auth.guard.ts
@@ -1,32 +1,34 @@
-import {CanActivateFn, Router} from '@angular/router';
-import {inject} from "@angular/core";
-import {AuthService} from "../services/auth.service";
-import {JwtService} from "../services/jwt.service";
+import { CanActivateFn, Router } from '@angular/router';
+import { inject } from "@angular/core";
+import { AuthService } from "../services/auth.service";
+import { JwtService } from "../services/jwt.service";
+import { Observable, of, switchMap } from 'rxjs';
+import {catchError} from "rxjs/operators";
 
-export const authGuard: CanActivateFn = (route, state) => {
+export const authGuard: CanActivateFn = (route, state): Observable<boolean> => {
   const authService = inject(AuthService);
   const jwtService = inject(JwtService);
   const router = inject(Router);
 
+  if (jwtService.getUserIsBlocked()) {
+    router.navigate(['/blocked']);
+    return of(false);
+  }
+
   if (jwtService.isTokenExpired()) {
-    authService.refreshToken().subscribe(
-      response => {
+    return authService.refreshToken().pipe(
+      switchMap(response => {
         console.log('Token refreshed:', response);
-        return true;
-      },
-      error => {
+        return of(true);
+      }),
+      catchError(error => {
         console.error('Token refresh failed:', error);
         authService.logout();
         router.navigate(['/login']);
-        return false;
-      }
+        return of(false);
+      })
     );
   }
 
-  if (jwtService.getUserIsBlocked()) {
-    router.navigate(['/blocked']);
-    return false;
-  }
-
-  return true;
+  return of(true);
 };
