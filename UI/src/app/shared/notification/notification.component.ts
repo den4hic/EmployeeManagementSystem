6864311b97db89b51bd7771cb274ddfa0ac35d80
Denys <gusardavydov2004@gmail.com>
2024-10-14 02:15:30 +0300

    rework style for notifications

--- UI/src/app/shared/notification/notification.component.ts
+++ UI/src/app/shared/notification/notification.component.ts
@@ -1,25 +1,35 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { MatSnackBar } from '@angular/material/snack-bar';
 import { Subscription } from 'rxjs';
+import { trigger, transition, style, animate } from '@angular/animations';
 import {SignalRService} from "../../services/signal-r.service";
 import {NotificationType} from "../../services/enums/notification-type";
 
 @Component({
   selector: 'app-notification',
-  template: '',
+  templateUrl: './notification.component.html',
+  styleUrl: './notification.component.css',
+  animations: [
+    trigger('notificationAnimation', [
+      transition(':enter', [
+        style({ opacity: 0, transform: 'translateY(-20px)' }),
+        animate('300ms', style({ opacity: 1, transform: 'translateY(0)' })),
+      ]),
+      transition(':leave', [
+        animate('300ms', style({ opacity: 0, transform: 'translateY(-20px)' })),
+      ]),
+    ]),
+  ],
 })
 export class NotificationComponent implements OnInit, OnDestroy {
   private notificationSubscription: Subscription = new Subscription();
+  notifications: any[] = [];
 
-  constructor(
-    private signalRService: SignalRService,
-    private snackBar: MatSnackBar
-  ) {}
+  constructor(private signalRService: SignalRService) {}
 
   ngOnInit() {
     this.notificationSubscription = this.signalRService.notifications.subscribe(
       (notificationType: NotificationType) => {
-        this.showNotification(notificationType);
+        this.addNotification(notificationType);
       }
     );
   }
@@ -30,38 +40,69 @@ export class NotificationComponent implements OnInit, OnDestroy {
     }
   }
 
-  private showNotification(notificationType: NotificationType) {
+  private addNotification(notificationType: NotificationType) {
+    const notification = this.createNotification(notificationType);
+    this.notifications.unshift(notification);
+    setTimeout(() => this.removeNotification(notification), 5000);
+  }
+
+  private createNotification(notificationType: NotificationType) {
+    let title = '';
     let message = '';
-    let action = 'Close';
-    let duration = 5000;
+    let icon = '';
+    let notificationClass = '';
 
     switch (notificationType) {
       case NotificationType.AssignedToTask:
+        title = 'New Task Assignment';
         message = 'You have been assigned to a new task.';
+        icon = 'fas fa-tasks';
+        notificationClass = 'notification-info';
         break;
       case NotificationType.TaskStatusChanged:
+        title = 'Task Status Updated';
         message = 'A task status has been updated.';
+        icon = 'fas fa-sync';
+        notificationClass = 'notification-warning';
         break;
       case NotificationType.TaskDeleted:
-        message = 'A task you are assigned to has been deleted.';
+        title = 'Task Deleted';
+        message = 'A task has been deleted.';
+        icon = 'fas fa-trash-alt';
+        notificationClass = 'notification-danger';
         break;
       case NotificationType.TaskCreated:
+        title = 'New Task Created';
         message = 'A new task has been created.';
+        icon = 'fas fa-plus-circle';
+        notificationClass = 'notification-success';
         break;
       case NotificationType.TaskUpdated:
+        title = 'Task Updated';
         message = 'A task has been updated.';
+        icon = 'fas fa-edit';
+        notificationClass = 'notification-info';
         break;
       case NotificationType.TaskDueDateChanged:
+        title = 'Task Due Date Changed';
         message = 'A task due date has been changed.';
+        icon = 'fas fa-calendar-alt';
+        notificationClass = 'notification-warning';
         break;
       default:
+        title = 'New Notification';
         message = 'You have a new notification.';
+        icon = 'fas fa-bell';
+        notificationClass = 'notification-info';
     }
 
-    this.snackBar.open(message, action, {
-      duration: duration,
-      horizontalPosition: 'end',
-      verticalPosition: 'top',
-    });
+    return { title, message, icon, class: notificationClass };
+  }
+
+  removeNotification(notification: any) {
+    const index = this.notifications.indexOf(notification);
+    if (index > -1) {
+      this.notifications.splice(index, 1);
+    }
   }
 }
