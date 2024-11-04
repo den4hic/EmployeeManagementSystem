7527a180cfcbe9517408a793a801dcb1d256af99
Denys <gusardavydov2004@gmail.com>
2024-11-04 02:49:10 +0200

    update base URL

--- UI/src/app/config/api.config.ts
+++ UI/src/app/config/api.config.ts
@@ -1,3 +1,3 @@
 export const API_CONFIG = {
-  baseUrl: 'https://employee-system-api.azurewebsites.net/api'
+  baseUrl: 'https://employee-system-api-fre4cmdvake3dea2.westeurope-01.azurewebsites.net/api'
 };
